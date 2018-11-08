9945ace349c0d2f7d9a7e84f9a9c17ffee87b3a5
Valentina Clavijo <valentina.clavijo.@alumnos.uv.cl>
2018-11-08 17:25:52 -0300

    Update sockets/echo_example/echoServerSrc/TCPEchoServer.cc

---
+++ sockets/echo_example/echoServerSrc/TCPEchoServer.cc
@@ -0,0 +1,73 @@
+/*
+ *   C++ sockets on Unix and Windows
+ *   Copyright (C) 2002
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "YASL.h"      // For Socket, ServerSocket, and SocketException
+#include <iostream>    // For cerr and cout
+#include <cstdlib>     // For atoi()
+
+const uint32_t RCVBUFSIZE = 32;    // Size of receive buffer
+
+// TCP client handling function
+void HandleTCPClient(TCPSocket *sock) {
+  std::cout << "Handling client ";
+  try {
+    std::cout << sock->getForeignAddress() << ":";
+  } catch (SocketException e) {
+    std::cerr << "Unable to get foreign address" << std::endl;
+  }
+  try {
+    std::cout << sock->getForeignPort();
+  } catch (SocketException e) {
+    std::cerr << "Unable to get foreign port" << std::endl;
+  }
+  std::cout << std::endl;
+
+  // Send received string and receive again until the end of transmission
+  char echoBuffer[RCVBUFSIZE];
+  uint32_t recvMsgSize;
+  while ((recvMsgSize = sock->recv(echoBuffer, RCVBUFSIZE)) > 0) { // Zero means
+                                                         // end of transmission
+    // Echo message back to client
+    sock->send(echoBuffer, recvMsgSize);
+  }
+  delete sock;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {                     // Test for correct number of arguments
+    std::cerr << "Usage: " << argv[0] << " <Server Port>" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  uint16_t echoServPort = atoi(argv[1]);  // First arg: local port
+
+  try {
+    TCPServerSocket servSock(echoServPort);     // Server Socket object
+  
+    for (;;) {   // Run forever
+      HandleTCPClient(servSock.accept());       // Wait for a client to connect
+    }
+  } catch (SocketException &e) {
+    std::cerr << e.what() << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  // NOT REACHED
+
+  return EXIT_SUCCESS;
+}
